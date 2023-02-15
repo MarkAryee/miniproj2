c3d2b494b8ff757cdcc5079f8239a9bbbef80304
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2023-02-15 20:44:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ console.log('something is happening here');
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
